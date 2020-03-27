9919b5512beab4281f6fde98463e2a366efef228
FedericoDeBona <federicodebona@hotmail.it>
2020-03-27 09:54:35 +0100

    .

---
+++ 2020-03-25 - transazioni sql.sql
@@ -0,0 +1,33 @@
+/*
+Esempio:
+Supponiamo di voler transferire una qta. di denaro da un conto corrente A ad un conto corrente B
+1. controllare che ci siano almen 500E nel C/C A
+2. togliere 500E dal C/C A
+3. aggiungere 500E al C/C B
+
+Quindi un TRANSAZIONE SQL è una sequenza di comand SQL che vengoono considerati
+ATOMICI nel senso he ii comandi vengono eseguiti uno dopo l'altro, ma dal punto
+di vista logico vengono considerati come un'unica operazione che si conclude con
+due possibili stati: SUCCESS / FAILURE.
+In caso di success, le modifiche vengono rese effettive nel database
+In caso di failure, le modifiche vengono annullate e si ritorna alla situazione che
+c'era prima dell'inizio della transazione
+
+START TRANSACTION: comando SQL che comunica al DBMS l'inizio di una nuova transazione
+
+COMMIT: comando per confermare e rendere definitive le modifiche realizzate all'interno
+		della transazione
+
+ROLLBACK: comando per ripristinare la situazione che c'era all'inizio transazione
+
+
+START TRANSACTION;
+comando sql 1;
+comando sql 2;
+...
+COMMIT
+oppure: ROLLBACK se intercetto una condizione di errore
+
+Nel corso della transazione, ogni utente vede le modifiche che sta facendo, ma queste non 
+non sono visibili da altri utenti collegati al database
+*/
